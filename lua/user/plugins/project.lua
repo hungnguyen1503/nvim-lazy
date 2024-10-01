900b6cdd4856951af247a3502efa2044e2f8ec8f
Hung Nguyen <hung.nguyen.jx@renesas.com>
2024-10-01 22:45:19 +0700

    Update dashboard_mode for project

--- lua/user/plugins/project.lua
+++ lua/user/plugins/project.lua
@@ -30,8 +30,12 @@ local options = {
     projects = project_dir,
     -- Path to store history and sessions
     datapath = vim.fn.stdpath("data"), -- ~/.local/share/nvim/
-    -- Load the most recent session on startup if not in the project directory
-    last_session_on_startup = false,
+    -- Dashboard mode prevent session autoload on startup
+    dashboard_mode = true,
+    -- Timeout in milliseconds before trigger FileType autocmd after session load
+    -- to make sure lsp servers are attached to the current buffer.
+    -- Set to 0 to disable triggering FileType autocmd
+    filetype_autocmd_timeout = 200,
 
     -- Overwrite some of Session Manager options
     session_manager_opts = {
