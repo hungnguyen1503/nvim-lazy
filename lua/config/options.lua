27e82ca3038b4a5cf49ae395b3257557806146a4
Hung Nguyen <hung.nguyen.jx@renesas.com>
2024-10-16 23:20:39 +0700

    Update neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -96,25 +96,16 @@ if vim.g.neovide then
     vim.g.neovide_touch_drag_timeout = 0
     vim.g.neovide_cursor_antialiasing = true
     vim.g.neovide_scroll_animation_length = 0.4
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_cursor_animate_command_line = true
     vim.g.neovide_cursor_vfx_particle_density = 0.2
+    vim.g.neovide_cursor_trail_size = 0.8
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_cursor_animate_in_insert_mode = false
     vim.g.neovide_cursor_animate_command_line = true
     vim.g.neovide_fullscreen = false
-
+    vim.g.neovide_cursor_smooth_blink = true
     vim.g.neovide_floating_blur_amount_x = 0.0
     vim.g.neovide_floating_blur_amount_y = 0.0
-    --
     vim.g.neovide_cursor_unfocused_outline_width = 0
-    -- global.neovide_cursor_vfx_particle_lifetime = 1.2
-
-    -- vim.g.neovide_cursor_vfx_particle_curl = 1.0
-    vim.g.neovide_cursor_vfx_particle_density = 0
-    -- vim.g.neovide_cursor_vfx_particle_speed = 10.0
-    -- vim.g.neovide_cursor_vfx_particle_phase = 1.5
-    -- vim.g.neovide_cursor_vfx_mode = "railgun"
-    -- vim.g.neovide_padding_top = 20
-    -- vim.g.neovide_padding_bottom = 20
-    -- vim.g.neovide_padding_right = 20
-    -- vim.g.neovide_padding_left = 20
 end
